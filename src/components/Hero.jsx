ab55e12c0a0f45a99eaa954fe1cf713975d8d51a
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-08 00:23:05 +0100

    ..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <div>
-        <section className="hero" id="home">
+      <section className="hero" id="home">
         <div className="container">
-
           <div className="hero-content">
-
             <p className="hero-subtitle">
               <ion-icon name="home"></ion-icon>
 
@@ -17,20 +15,24 @@ const Hero = () => {
             <h2 className="h1 hero-title">Find Your Dream House By Us</h2>
 
             <p className="hero-text">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore.
             </p>
 
             <button className="btn">Make An Enquiry</button>
-
           </div>
 
           <figure className="hero-banner">
-            <img src="./hero-banner.png" alt="Modern house model" className="w-100" />
+            <img
+              src="./hero-banner.png"
+              alt="Modern house model"
+              className="w-100"
+            />
           </figure>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
