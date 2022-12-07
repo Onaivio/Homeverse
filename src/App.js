3c56b7fc4f8ecdd69606a38143475326d9808566
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-08 00:46:41 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from'react';
-import { Navbar, Hero, About, Service, Property } from './components/'
+import { Navbar, Hero, About, Service, Property, Feature } from './components/'
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <About />
       <Service />
       <Property />
+      <Feature />
     </>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Hero } from './Hero';
 export { default as About } from './About'
 export { default as Service } from './Service'
 export { default as Property } from './Property'
+export { default as Feature } from './Feature'
