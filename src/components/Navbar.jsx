2a57dcdd18d17aab323c20325f26845029587c94
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-08 00:54:34 +0100

    ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Navbar = () => {
-  const header = document.querySelector("[data-header]");
+    const header = document.querySelector("[data-header]");
 
-  window.addEventListener("scroll", function () {
-    window.scrollY >= 400 ? header.classList.add("active")
-      : header.classList.remove("active");
-  }); 
+    window.addEventListener("scroll", function () {
+      window.scrollY >= 400 ? header.classList.add("active")
+        : header.classList.remove("active");
+    }); 
 
   return (
     <>
