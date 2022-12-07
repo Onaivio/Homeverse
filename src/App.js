153cf201023a810a917554540590745a71d305c7
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-08 00:20:59 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from'react';
-import { Navbar, Hero } from './components/'
+import { Navbar, Hero, About } from './components/'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   );
 }
