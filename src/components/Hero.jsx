a53ecbf858b6ae9a251e4a32f006d6d4254f359c
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-08 00:16:30 +0100

    ..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,29 +3,29 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <section class="hero" id="home">
-        <div class="container">
+        <section className="hero" id="home">
+        <div className="container">
 
-          <div class="hero-content">
+          <div className="hero-content">
 
-            <p class="hero-subtitle">
+            <p className="hero-subtitle">
               <ion-icon name="home"></ion-icon>
 
               <span>Real Estate Agency</span>
             </p>
 
-            <h2 class="h1 hero-title">Find Your Dream House By Us</h2>
+            <h2 className="h1 hero-title">Find Your Dream House By Us</h2>
 
-            <p class="hero-text">
+            <p className="hero-text">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
             </p>
 
-            <button class="btn">Make An Enquiry</button>
+            <button className="btn">Make An Enquiry</button>
 
           </div>
 
-          <figure class="hero-banner">
-            <img src="./assets/images/hero-banner.png" alt="Modern house model" class="w-100" />
+          <figure className="hero-banner">
+            <img src="./assets/images/hero-banner.png" alt="Modern house model" className="w-100" />
           </figure>
 
         </div>
