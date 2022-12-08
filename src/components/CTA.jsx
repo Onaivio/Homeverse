bcf3d5a71b1ba22775748378dad7497636284c3c
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-08 01:09:07 +0100

    ..

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const CTA = () => {
   return (
     <>
-    <section class="cta">
+      <section class="cta">
         <div class="container">
-
           <div class="cta-card">
             <div class="card-content">
               <h2 class="h2 card-title">Looking for a dream home?</h2>
 
-              <p class="card-text">We can help you realize your dream of a new home</p>
+              <p class="card-text">
+                We can help you realize your dream of a new home
+              </p>
             </div>
 
             <button class="btn cta-btn">
@@ -19,11 +20,10 @@ const CTA = () => {
               <ion-icon name="arrow-forward-outline"></ion-icon>
             </button>
           </div>
-
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
