f8a812ab2b5db5db226c9c637b12d12b3cb0c93c
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-08 01:11:26 +0100

    ..

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const CTA = () => {
   return (
     <>
-      <section class="cta">
-        <div class="container">
-          <div class="cta-card">
-            <div class="card-content">
-              <h2 class="h2 card-title">Looking for a dream home?</h2>
+      <section className="cta">
+        <div className="container">
+          <div className="cta-card">
+            <div className="card-content">
+              <h2 className="h2 card-title">Looking for a dream home?</h2>
 
-              <p class="card-text">
+              <p className="card-text">
                 We can help you realize your dream of a new home
               </p>
             </div>
 
-            <button class="btn cta-btn">
+            <button className="btn cta-btn">
               <span>Explore Properties</span>
 
               <ion-icon name="arrow-forward-outline"></ion-icon>
